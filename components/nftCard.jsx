1f02ad5af12989d681e898db72684cfaf1f2b563
toto <50374594+toine08@users.noreply.github.com>
2022-05-27 15:49:45 +0200

    init project

---
+++ components/nftCard.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+export const NFTCard = ({nft, loading}) => {
+
+    return(
+        <div className="w-1/4 flex flex-col rounded-md border-2 border-black">
+            <div className="rounded-md border-b-1 border-black">
+            <img key={nft.media} className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+
+            </div>
+            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110">
+                <div className="">
+                    <h2 key={nft.title} className="text-xl text-gray-800">{nft.title}</h2>
+                    <p key={nft.id.tokenId} className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length -4)}</p>
+                    <p key={nft.contract.address} onClick={() => {navigator.clipboard.writeText(nft.contract.address)}} className="text-gray-600">{`${nft.contract.address.substr(0,5)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+                </div>
+
+                <div className="flex-grow mt-2">
+                    <p key={nft.description} className="text-gray-600">{nft.description.substr(0,150)}...</p>
+                </div>
+                <div className='flex-grow mt-2 '>
+                    <a className='text-blue-800 justify-center' target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`}>View on etherscan</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+/*.substr(0,150)*/
+
+/**
+ * 
+ */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,107 @@
+import { ConstructorExpression } from 'assemblyscript'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {useState} from 'react'
+import {NFTCard} from '../components/nftCard';
+import Pagination from '../components/Pagination';
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const api_key = "1XC8ix6qPvu-kQSq3KTHDgDCY-WiOVMm"
+const Home = () => {
+  const [wallet, setWalletAddress] = useState("");
+  const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([])
+  const [fetchForCollection, setFetchForCollection] = useState(false)
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+  //pagination system
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nftsPerPage] = useState(10);
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+  const fetchNFTsForCollection = async () => {
+    if (collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
+      const api_key = "1XC8ix6qPvu-kQSq3KTHDgDCY-WiOVMm"
+      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      if (nfts) {
+        console.log("NFTs in collection:", nfts)
+        setNFTs(nfts.nfts)
+      }
+    }
+  }
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+  const fetchNFTs = async() => {
+    let nfts; 
+    console.log("fetching nfts");
+    const api_key = "1XC8ix6qPvu-kQSq3KTHDgDCY-WiOVMm"
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
+    var requestOptions = {
+        method: 'GET'
+      };
+     
+    if (!collection.length) {
+    
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+  
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    } else {
+      console.log("fetching nfts for collection owned by address")
+      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+      nfts= await fetch(fetchURL, requestOptions).then(data => data.json())
+    }
+  
+    if (nfts) {
+      console.log("nfts:", nfts)
+      setNFTs(nfts.ownedNfts)
+    }
+  }
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+   //get current nfts
+   const indexOfLastPost = currentPage * nftsPerPage
+   const indexOfFirstPost = indexOfLastPost - nftsPerPage
+   const currentNFTs = NFTs.slice(indexOfFirstPost, indexOfLastPost)
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+   const paginate = (pageNumber) => setCurrentPage(currentPage + 1)
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+
+  return(
+
+      <div className='flex flex-col items-center justify-center py-8 gap-y-3 '>
+        <h1 className="text-3xl m-4 font-semibold underline">NFT Watchers</h1>
+        <div className='flex flex-col w-full justify-center items-center gap-y-2'>
+          <input disabled={fetchForCollection} onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} type={"text"} placeholder="add your wallet address" className='w-2/5 border-2 border-black rounded-md p-2' ></input>
+          <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="add the collection address" className='w-2/5 border-2 border-black rounded-md p-2'></input>
+          <label className="text-gray-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+          <button className={"disabled:bg-slate-500 text-white bg-black rounded-full px-4 py-2 mt-3 w-1/5"} onClick={
+           () => {
+             if(fetchForCollection){
+               fetchNFTsForCollection();
+             }else fetchNFTs();
+          }
+        }>Let's go! </button>
         </div>
-      </main>
+        <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+        {NFTs.length > 0 &&
+            currentNFTs.map((nft) => {
+              return <NFTCard nft={nft} loading={loading}/>
+            })}
+        </div>
+        <div>
+          {NFTs.length > 0 && (
+            <Pagination
+              nftsPerPage={nftsPerPage}
+              totalNFTs={NFTs.length}
+              paginate={paginate}
+              currentPage={currentNFTs}
+            />
+          )}
+        </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+      </div>
   )
 }
-
 export default Home
